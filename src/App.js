7926a0cb19db3507c48b4b2094f4d78c48808740
Amruth Lakya Sannaiah <33801@ITCINFOTECH.COM>
2019-12-26 15:12:28 +0530

    unit test for App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {props.showLink ? <a>show link</a> : <a>hide link</a>}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Count : {count}</h1>
+        <div className="btn-container">
+          <button className="counter-btn increment" onClick={() => setCount(count + 1)}>Increase count</button>
+          <button className="counter-btn decrement" onClick={() => setCount(count + -1)}>Decrease count</button>
+          <button className="counter-btn reset" onClick={() => setCount(0)}>Reset count</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import { exact } from 'prop-types';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App component', () => {
+  it('Renders without props', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+  it('Renders with props', () => {
+    const props = {
+      showLink: true
+    }
+    const div = document.createElement('div');
+    ReactDOM.render(<App {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+  it('Renders hide link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/hide link/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  it('Renders show link', () => {
+    const props = {
+      showLink: true
+    }
+    const { getByText } = render(<App {...props} />);
+    const linkElement = getByText(/show link/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  it('Renders when state counter value with 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count : 0');
+  });
+  it('Renders when state counter value is increased when button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count : 1');
+  })
+  it('Renders when state is decremented', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.decrement');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count : -1');
+  })
+  it('Renders when state is reset', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.reset');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Count : 0');
+  })
+  it('Renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
